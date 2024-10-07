5e13f3318facb7837c8aee6bb3d6fd81430738cc
1matux1 <mateusz.kmak@onet.pl>
2024-10-07 12:24:44 +0200

    Sortowanie Przez Wstawianie

--- Testowanie/Testowanie/Program.cs
+++ Testowanie/Testowanie/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace Testowanie
 {
-    internal class Program
+    class SortowanieWstawianie
+    {
+        public static void Sort(int[] array)
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    int temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+                }
+            }
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
+            int[] arr = { 64, 25, 12, 22, 11 };
+            SortowanieWstawianie.Sort(arr);
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey(true);
         }
     }
 }
